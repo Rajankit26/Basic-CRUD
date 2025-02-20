8b50bd75ac86cbeece194c2140081362abf0db4a
Ankit <rajankit26smp@gmail.com>
2025-02-20 22:54:55 +0530

    created userSchema

---
+++ model/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name :{
+        type : String,
+        required :['true',"Name is Required"],
+        trim : true,
+        maxLength : [10, "Name must be less than 10 chars"],
+    },
+    email : {
+        type : String,
+        required : ['true',"Email is required"],
+        unique : true,
+    },
+    age : {
+        type : Number,
+        required : ['true',"Age is required"]
+    }
+});
+
+
+module.exports = mongoose.model("User",userSchema);

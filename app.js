6a450275d779322386a8dd3095b6e549c33a9696
Ankit <rajankit26smp@gmail.com>
2025-02-20 23:59:52 +0530

    DB connected with env

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ require ("dotenv").config()
 const express = require("express")
 const app = express();
 
+const connectToDB = require("./config/db.js")
+
+
 app.get('/', (req,res) =>{
     res.send("Hello from Server!")
 })
 
+
+// Initialising connection to DB
+connectToDB();
 
 module.exports = app;
 
--- config/db.js
+++ config/db.js
@@ -1,2 +1,14 @@
 
+const mongoose = require("mongoose");
 
+const connectToDB = async() => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`connected to DB Successfully! : ${connect.connection.host}`);
+    } catch (error) {
+        console.error(`Error :${error}`);
+        process.exit(1);
+    }
+};
+
+module.exports = connectToDB;
